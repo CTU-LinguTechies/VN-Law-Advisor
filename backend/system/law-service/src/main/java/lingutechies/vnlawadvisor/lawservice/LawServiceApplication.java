42dde77aea452b4b470821e03963b27d99ef5db9
TranHuy2k2 <giahuytran2002@gmail.com>
2023-11-24 14:28:49 +0700

    chore: init law-service

---
+++ backend/system/law-service/src/main/java/lingutechies/vnlawadvisor/lawservice/LawServiceApplication.java
@@ -0,0 +1,13 @@
+package lingutechies.vnlawadvisor.lawservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LawServiceApplication.class, args);
+	}
+
+}
