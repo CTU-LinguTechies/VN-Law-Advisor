455f9078852bc521f914155586ed52a92a22ded0
TranHuy2k2 <giahuytran2002@gmail.com>
2023-11-27 01:43:34 +0700

    chore: initialize next app

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+import StyledComponentsRegistry from '../lib/AntdRegistry';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+    title: 'VN Law Advisor',
+    description: 'Hỏi đáp tri thức pháp luật Việt Nam',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+            </body>
+        </html>
+    );
+}
